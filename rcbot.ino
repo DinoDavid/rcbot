2a241be8b459b24820b0613f86ded5ee90c923a1
DinoDavid <DinoDavid@users.noreply.github.com>
2016-11-19 19:47:44 +0100

    Lights and BT state
    Implementation of lights and bluetooth state pin in the code

--- rcbot.ino
+++ rcbot.ino
@@ -1,17 +1,27 @@
+char incomingByte;
+
+//L298P Connection  
 #define E1 5  // Enable Pin for motor 1
 #define E2 6  // Enable Pin for motor 2
 
 #define M1 4  // Control pin 1 for motor 1
-#define M2 7  // Control pin 2 for motor 1
+#define M2 7  // Control pin 2 for motor 2
 
+//Leds connected to Arduino UNO Pin 9
+const int light  = 9;
+int l = 0;
+
+//Bluetooth (HC-05) State pin connected to Arduino UNO Pin 8
+const int BTState = 8;
+  
 void forward() {
   digitalWrite(E1, HIGH);
-  digitalWrite(M1, LOW);
+  digitalWrite(M1, HIGH);
 }
 
 void backward() {
   digitalWrite(E1, HIGH);
-  digitalWrite(M1, HIGH);
+  digitalWrite(M1, LOW);
 }
 
 void halt() {
@@ -37,10 +47,17 @@ void setup() {
     pinMode(M1, OUTPUT);   
     pinMode(M2, OUTPUT); 
 
+    pinMode(light, OUTPUT); 
+    pinMode(BTState, INPUT);
+
     Serial.begin(9600);
 }
 
 void loop() {
+//Stop car when connection lost or bluetooth disconnected
+  if(digitalRead(BTState)==LOW)
+    halt();
+  
   int c = Serial.read();
 
   if (c == 'F' || c == 'G' || c == 'I')
@@ -51,7 +68,20 @@ void loop() {
     left();
   if (c == 'I' || c == 'J' || c == 'R')
     right();
-
+  
+//Light    
+  if (c == 'W'){
+    if (l == 0){
+      digitalWrite(light, HIGH);
+      l=1;
+    }
+    else if (l == 1){
+      digitalWrite(light, LOW); 
+      l=0;
+    }
+    c='w';
+  }
+    
   switch(c) {
     case 'F':
     case 'B':
@@ -64,7 +94,6 @@ void loop() {
     case -1:
       break; /* Fjern denne for a bruke spam-metoden. */
     default: /* Fanger release signal hvis ^ er ufjernet. */
-      halt();
+      halt();    
   }
-  delay(10);
 }
